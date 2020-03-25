6a7e1db2c2eaae8b6230dc38a8ba4e5d7a9ca6c6
jerome <jerometonnelier@yahoo.fr>
2020-03-25 13:58:13 +0100

    Initial Commit

---
+++ CovidApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CovidApp
+//
+//  Created by jerome on 25/03/2020.
+//  Copyright © 2020 Jerome TONNELIER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
